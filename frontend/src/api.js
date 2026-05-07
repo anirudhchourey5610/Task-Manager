9743d9d45520f4d97b6ea18276fc522987bbe300
Anirudh Chourey <anirudhchourey@Anirudhs-MacBook-Air.local>
2026-05-07 12:50:24 +0530

    Fixed frontend API baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: "https://backend-production-ca89b.up.railway.app/api",
+   baseURL: "https://backend-production-ca89b.up.railway.app/api/",
 });
 
 // Interceptor to automatically add userId header to every request dynamically
